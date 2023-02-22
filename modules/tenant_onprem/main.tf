2a76043163be0e185f28c3f3cf925de9c9849184
Mark Sibering <mark.sibering@devoteam.com>
2023-02-22 13:53:31 +0100

    fix wrong link

--- modules/tenant_onprem/main.tf
+++ modules/tenant_onprem/main.tf
@@ -3,12 +3,12 @@ variable "location" {}
 
 resource "null_resource" "tenant" {
   triggers = {
-    value = var.name
+    value = var.environment.name
   }
 
   provisioner "local-exec" {
     command = <<EOT
-      echo "Create tenant ${var.name}"
+      echo "Create tenant ${var.environment.name}"
     EOT
   }
 }
